dcf0cab8c6156d50374b2992ba5c1d6202ecb891
william rockel <wrockel@gmail.com>
2025-06-06 04:57:08 +0000

    It's not working. Can you think of three ways it wouldn't work and how t

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
       </head>
       <body className="font-body antialiased flex flex-col min-h-screen">
         <Header />
