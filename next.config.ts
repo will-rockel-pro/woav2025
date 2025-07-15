a6275da3ae1b48903dddc536f2a86aa8294d5e53
william rockel <wrockel@gmail.com>
2025-07-15 13:00:00 +0000

    I deleted the nested directory

--- next.config.ts
+++ next.config.ts
@@ -24,7 +24,7 @@ const nextConfig: NextConfig = {
         pathname: '/s2/favicons/**',
       },
       {
-        protocol: 'https',
+        protocol: 'https' as const,
         hostname: 'firebasestorage.googleapis.com',
         port: '',
         pathname: '/**',
